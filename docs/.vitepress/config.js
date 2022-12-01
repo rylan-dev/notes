aafd778357502155f57b09d2c1dcb31c1aab3e2b
rylan-dev <2459493196@qq.com>
2022-12-01 21:11:13 +0800

    feat: suport latex

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,14 +6,15 @@ import { SideBarList } from "./SideBar";
 export default defineConfig({
   head: [
     ["link", { rel: "icon", href: "/avatar.jpg" }],
+    ["link", { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css" }],
   ],
   base: "/",
   title: "夕漫的小宇宙",
   description: "前端学习记录",
   markdown: {
-    theme: {
-      light: "vitesse-light",
-      dark: "vitesse-dark",
+    theme: "material-palenight",
+    config: (md) => {
+      md.use(require("markdown-it-katex"));
     },
   },
   themeConfig: {
